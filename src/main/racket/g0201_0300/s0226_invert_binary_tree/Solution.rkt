d63841097e8836ef7422d7e47bff3c8fc2deab74
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2025-02-10 23:39:50 +0700

    Updated task 226

--- src/main/racket/g0201_0300/s0226_invert_binary_tree/Solution.rkt
+++ src/main/racket/g0201_0300/s0226_invert_binary_tree/Solution.rkt
@@ -1,26 +1,28 @@
 ; #Easy #Top_100_Liked_Questions #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree
 ; #Data_Structure_I_Day_12_Tree #Level_2_Day_6_Tree #Udemy_Tree_Stack_Queue
 ; #Top_Interview_150_Binary_Tree_General #Big_O_Time_O(n)_Space_O(n)
-; #2025_02_10_Time_0_ms_(100.00%)_Space_129.49_MB_(100.00%)
+; #2025_02_10_Time_0_ms_(100.00%)_Space_102.41_MB_(0.00%)
 
 ; Definition for a binary tree node.
 #|
-
 ; val : integer?
 ; left : (or/c tree-node? #f)
 ; right : (or/c tree-node? #f)
 (struct tree-node
   (val left right) #:mutable #:transparent)
 
-; constructor
-(define (make-tree-node [val 0])
-  (tree-node val #f #f))
-
+; Helper function to create a tree node.
 |#
+(define (make-tree-node val [left #f] [right #f])
+  (tree-node val left right))
 
-(define/contract (count-nodes root)
-  (-> (or/c tree-node? #f) exact-integer?)
-  (if (not root)
-      0
-      (+ 1 (count-nodes (tree-node-left root))
-           (count-nodes (tree-node-right root)))))
+; Function to invert a binary tree.
+(define/contract (invert-tree root)
+  (-> (or/c tree-node? #f) (or/c tree-node? #f))
+  (cond
+    [(not root) #f] ; Base case: empty tree.
+    [else
+     (make-tree-node
+      (tree-node-val root)
+      (invert-tree (tree-node-right root))
+      (invert-tree (tree-node-left root)))]))
