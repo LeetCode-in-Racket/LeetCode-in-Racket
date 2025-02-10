086be21d3b08fedc17eb8bb3a50b7335e97ea3ba
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-10 16:28:49 +0200

    Improved tags

--- src/main/racket/g0201_0300/s0226_invert_binary_tree/Solution.rkt
+++ src/main/racket/g0201_0300/s0226_invert_binary_tree/Solution.rkt
@@ -1,5 +1,7 @@
-; #Easy #Depth_First_Search #Tree #Binary_Search #Binary_Tree #Binary_Search_II_Day_10
-; #Top_Interview_150_Binary_Tree_General #2025_02_10_Time_0_(100.00%)_Space_129.49_(100.00%)
+; #Easy #Top_100_Liked_Questions #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree
+; #Data_Structure_I_Day_12_Tree #Level_2_Day_6_Tree #Udemy_Tree_Stack_Queue
+; #Top_Interview_150_Binary_Tree_General #Big_O_Time_O(n)_Space_O(n)
+; #2025_02_10_Time_0_ms_(100.00%)_Space_129.49_MB_(100.00%)
 
 ; Definition for a binary tree node.
 #|
--- src/main/racket/g0201_0300/s0300_longest_increasing_subsequence/Solution.rkt
+++ src/main/racket/g0201_0300/s0300_longest_increasing_subsequence/Solution.rkt
@@ -1,7 +1,7 @@
 ; #Medium #Top_100_Liked_Questions #Array #Dynamic_Programming #Binary_Search
 ; #Algorithm_II_Day_16_Dynamic_Programming #Binary_Search_II_Day_3 #Dynamic_Programming_I_Day_18
 ; #Udemy_Dynamic_Programming #Top_Interview_150_1D_DP #Big_O_Time_O(n*log_n)_Space_O(n)
-; #2025_02_10_Time_3_(100.00%)_Space_101.78_(100.00%)
+; #2025_02_10_Time_3_ms_(100.00%)_Space_101.78_MB_(100.00%)
 
 (require racket/list)
 
