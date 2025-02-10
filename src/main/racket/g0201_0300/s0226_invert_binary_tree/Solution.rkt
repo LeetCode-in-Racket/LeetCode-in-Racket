380a55dc60b7d2076b71dd75807e4034c5932263
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-10 18:39:17 +0200

    Update Solution.rkt

--- src/main/racket/g0201_0300/s0226_invert_binary_tree/Solution.rkt
+++ src/main/racket/g0201_0300/s0226_invert_binary_tree/Solution.rkt
@@ -1,7 +1,7 @@
 ; #Easy #Top_100_Liked_Questions #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree
 ; #Data_Structure_I_Day_12_Tree #Level_2_Day_6_Tree #Udemy_Tree_Stack_Queue
 ; #Top_Interview_150_Binary_Tree_General #Big_O_Time_O(n)_Space_O(n)
-; #2025_02_10_Time_0_(100.00%)_Space_101.20_(100.00%)
+; #2025_02_10_Time_0_ms_(100.00%)_Space_102.41_MB_(0.00%)
 
 ; Definition for a binary tree node.
 #|
@@ -23,6 +23,6 @@
     [(not root) #f] ; Base case: empty tree.
     [else
      (make-tree-node
-      (tree-node-val root)                   ; Root value remains the same.
-      (invert-tree (tree-node-right root))  ; Invert right subtree and assign to left.
-      (invert-tree (tree-node-left root)))])) ; Invert left subtree and assign to right.
+      (tree-node-val root)
+      (invert-tree (tree-node-right root))
+      (invert-tree (tree-node-left root)))]))
